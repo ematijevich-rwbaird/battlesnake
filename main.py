ec6abc2173ff8c9c86d05ad9f231b54b9bdb7a04
matavach <erik@matijevich.org>
2025-01-14 19:41:48 -0600

    body

--- main.py
+++ main.py
@@ -52,32 +52,76 @@ def move(game_state: typing.Dict) -> typing.Dict:
 
     if my_neck["x"] < my_head["x"]:  # Neck is left of head, don't move left
         is_move_safe["left"] = False
+        print("1")
 
     elif my_neck["x"] > my_head["x"]:  # Neck is right of head, don't move right
         is_move_safe["right"] = False
-
+        print("2")
     elif my_neck["y"] < my_head["y"]:  # Neck is below head, don't move down
         is_move_safe["down"] = False
-
+        print("3")
     elif my_neck["y"] > my_head["y"]:  # Neck is above head, don't move up
         is_move_safe["up"] = False
+        print("4")
 
     # TODO: Step 1 - Prevent your Battlesnake from moving out of bounds
-    # board_width = game_state['board']['width']
-    # board_height = game_state['board']['height']
+    board_width = game_state['board']['width']
+    board_height = game_state['board']['height']
+    if my_head['x'] == board_width - 1 :
+        is_move_safe["right"] = False
+        print("5")
+    if my_head['x'] == 0 :
+        is_move_safe["left"] = False
+        print("6")
+    if my_head['y'] == board_height - 1 :
+        is_move_safe["up"] = False
+        print("7")
+    if my_head['y'] == 0 :
+        is_move_safe["down"] = False
+        print("8")
 
     # TODO: Step 2 - Prevent your Battlesnake from colliding with itself
-    # my_body = game_state['you']['body']
+  
+
 
     # TODO: Step 3 - Prevent your Battlesnake from colliding with other Battlesnakes
-    # opponents = game_state['board']['snakes']
+    opponents = game_state['board']['snakes']
+    
+    for op in opponents :
+            if is_move_safe["right"] :
+                for piece in op['body']:
+                    if my_head['x'] == piece['x'] - 1 and my_head['y'] == piece['y']:
+                        is_move_safe['right'] = False
+                        print('9')
+                        break
+            if is_move_safe["left"] :
+                for piece in op['body']:
+                    if my_head['x'] == piece['x'] + 1 and my_head['y'] == piece['y']:
+                        is_move_safe['left'] = False
+                        print('10')
+                        break
+            if is_move_safe["up"] :
+                for piece in op['body']:
+                    if my_head['y'] == piece['y'] - 1 and my_head['x'] == piece['x']:
+                        is_move_safe['up'] = False
+                        print('11')
+                        break
+            if is_move_safe["down"] :
+                for piece in op['body']:
+                    if my_head['y'] == piece['y'] + 1 and my_head['x'] == piece['x']:
+                        is_move_safe['down'] = False
+                        print("12")
+                        break
+
 
     # Are there any safe moves left?
     safe_moves = []
     for move, isSafe in is_move_safe.items():
         if isSafe:
             safe_moves.append(move)
+    
 
+    print(safe_moves)
     if len(safe_moves) == 0:
         print(f"MOVE {game_state['turn']}: No safe moves detected! Moving down")
         return {"move": "down"}
@@ -97,3 +141,4 @@ if __name__ == "__main__":
     from server import run_server
 
     run_server({"info": info, "start": start, "move": move, "end": end})
+    
